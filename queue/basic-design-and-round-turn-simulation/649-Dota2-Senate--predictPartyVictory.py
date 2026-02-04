d0f25e7635a88a9ec591b3167472f4970544fc44
Tim Codex <nguyentien.jobs@gmail.com>
2026-02-03 18:01:56 -0800

    solve 649

--- queue/basic-design-and-round-turn-simulation/649-Dota2-Senate--predictPartyVictory.py
+++ queue/basic-design-and-round-turn-simulation/649-Dota2-Senate--predictPartyVictory.py
@@ -2,12 +2,32 @@ class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         D = 0
         R = 0
-        queue = senate.split()
-        changed = True
-        while changed:
-            changed = False
+        queue = list(senate)
+        remain = Counter(queue)        
+        pending = Counter()
+
+        def winnerNameFound():
+            if remain['R'] <= 0 and remain['D'] > 0: return 'Dire'
+            if remain['D'] <= 0 and remain['R'] > 0: return 'Radiant'
+            return ''
+        def oppositeName(cur_name: str) -> str:
+            return 'D' if cur_name == 'R' else 'R'
+        
+        while not winnerNameFound():
             qlen = len(queue)
             for i in range(qlen):
+                name = queue.pop(0)
+                if pending[name] > 0:
+                    pending[name] -= 1
+                    remain[name] -= 1
+                    # print('misturn', name, remain, pending)
+                    continue
+                pending[oppositeName(name)] += 1
+                queue.append(name)
+                # print('power', name, pending, queue)
+        return winnerNameFound()
+                
+
                 
         
 
