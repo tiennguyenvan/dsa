b6fe481e3d61379d61cfd542c8ec555f10e102bc
Tim Codex <nguyentien.jobs@gmail.com>
2026-02-03 16:19:52 -0800

    init

---
+++ queue/basic-design-and-round-turn-simulation/649-Dota2-Senate--predictPartyVictory.py
@@ -0,0 +1,14 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        D = 0
+        R = 0
+        queue = senate.split()
+        changed = True
+        while changed:
+            changed = False
+            qlen = len(queue)
+            for i in range(qlen):
+                
+        
+
+        
