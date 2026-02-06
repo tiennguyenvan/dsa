0a3ca623d877066b5e43bc70d47682812e34fee9
Tim Codex <nguyentien.jobs@gmail.com>
2026-02-06 07:45:24 -0800

    LC 107 Binary Tree Level Order Traversal II

---
+++ queue/BFS - Trees - Graphs - Grids/LC 107 Binary Tree Level Order Traversal II.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root: return []
+        q = deque([root])
+        ret = []
+        while q:        
+            l = len(q)
+            lvl = []
+            for _ in range(l):
+                node = q.popleft()
+                lvl.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ret.append(lvl)
+
+        ret.reverse()
+        return ret
