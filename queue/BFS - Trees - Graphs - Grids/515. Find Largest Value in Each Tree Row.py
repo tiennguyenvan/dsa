71584fedb4625a85a2c2c0736d3e593768f34dbc
Tim Codex <nguyentien.jobs@gmail.com>
2026-02-06 16:02:27 -0800

    LC 515 Find Largest Value in Each Tree Row

---
+++ queue/BFS - Trees - Graphs - Grids/515. Find Largest Value in Each Tree Row.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+        q = deque([root])
+        ret = []
+        while q:
+            l = len(q)
+            lvl = []
+            for _ in range(l):
+                node = q.popleft()
+                lvl.append(node.val)
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+            ret.append(max(lvl))
+        return ret
