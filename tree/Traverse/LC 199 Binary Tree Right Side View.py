a7f92caf02795d5f10a665a919022d1269e05a62
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-05-18 09:21:51 -0700

    LC 199 Binary Tree Right Side View.py

---
+++ tree/Traverse/LC 199 Binary Tree Right Side View.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        ret = []        
+        q = deque([root])
+        while root and q:
+            ql = len(q)
+            ret.append(q[-1].val)            
+            for _ in range(ql):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return ret
+
+            
