78a755c748cce9ab54fe97ab1e8b06d20b8274b9
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-05-22 08:14:55 -0700

    isValidBST v2

--- tree/BST/LC 98 Validate Binary Search Tree.py
+++ tree/BST/LC 98 Validate Binary Search Tree.py
@@ -4,7 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution_v1:    
+class Solution_v2: # learned from the accepted one
+    def dfs(self, root: Optional[TreeNode], lower: float, upper: float) -> bool:
+        if not root:
+            return True
+        if not (lower < root.val < upper):
+            return False
+        return (
+            self.dfs(root.left, lower, root.val) 
+            and self.dfs(root.right, root.val, upper)
+        )
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.dfs(root, float("-inf"), float("inf"))
+        
+
+class Solution_v1: # this has the same Time Complexity, totally acceptable
     def minMax(self, root: Optional[TreeNode]) -> (int,int):
         minVal = root.val
         maxVal = root.val        
