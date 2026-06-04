4a39d7dcffc166bddb086ebcc5d9157edc116e09
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-06-04 08:35:01 -0700

    LC 105 Construct Binary Tree from Preorder and Inorder Traversal

---
+++ tree/Construct/LC 105 Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        v2i = {}
+        for i,v in enumerate(inorder):
+            v2i[v] = i
+        n = len(preorder)        
+        def build(s,l,r):            
+            if l >= r:  
+                return None
+            v = preorder[s]
+            root = TreeNode(v)
+            m = v2i[v]          
+            ls = s+1
+            rs = s+1+m-l  
+            root.left = build(ls, l, m)                        
+            root.right = build(rs, m+1, r)            
+            return root
+        return build(0, 0, n)
+
+
+
+        # if not preorder or not inorder:
+        #     return None
+        # root = TreeNode(preorder[0])
+        # index = inorder.index(root.val)
+        # root.left = self.buildTree(preorder[1:index+1], inorder[:index])
+        # root.right = self.buildTree(preorder[index+1:], inorder[index+1:])
+        # return root
+        
