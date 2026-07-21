f0eefa6e4c30009acb506a8508356d675ed5446f
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-07-21 14:08:50 -0700

    560. Subarray Sum Equals K

--- array/Prefix Sum/LC 560 Subarray Sum Equals K.py
+++ array/Prefix Sum/LC 560 Subarray Sum Equals K.py
@@ -1,20 +1,15 @@
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        c = Counter()
-        c[0] = 1
-        t = 0
-        prefix = []
-        l = len(nums)
-        if l == 1:
-            return 1 if nums[0] == k else 0
-
+        pre = Counter()
+        pre[0] = 1
         ret = 0
-        for i, n in enumerate(nums):            
-            t+=n
-            ret += c[t - k]
-            c[t] +=1 
-            prefix.append(t)
-            # print(c, prefix)
-            # if n == k:
-            #     ret + 1
+        curSum = 0
+        for n in nums:
+            curSum += n
+            ret += pre[curSum-k]
+            pre[curSum]+=1
+
         return ret
+
+        
