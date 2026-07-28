9577769bcd69d4381bb487ea5723268e9951cb23
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-07-28 09:58:39 -0700

    LC 739 Daily Temperatures

--- stack/Monotoic Stack/LC 739 Daily Temperatures.py
+++ stack/Monotoic Stack/LC 739 Daily Temperatures.py
@@ -1,11 +1,15 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        ans = [0] * n
-        preTemp = []
+        stack = []
+        ret = [0] * len(temperatures)
         for i, t in enumerate(temperatures):
-            while preTemp and preTemp[-1][1] < t:
-                pI, _ = preTemp.pop()
-                ans[pI] = i - pI                
-            preTemp.append((i,t))
-        return ans
+            if not stack:
+                stack.append(i)
+                continue
+            
+            while stack and temperatures[stack[-1]] < t:
+                j = stack.pop()
+                ret[j] = i - j
+            stack.append(i)
+        return ret
+
