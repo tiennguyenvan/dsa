5c80782afcf6f7fc6bc4888f818d88906459595d
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-07-28 12:46:51 -0700

    LC 239 Sliding Window Maximum

--- queue/Deque - Monotonic - Window - Prefix/LC 239 Sliding Window Maximum.py
+++ queue/Deque - Monotonic - Window - Prefix/LC 239 Sliding Window Maximum.py
@@ -1,31 +1,16 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        dq = deque()
-        # init queue
-        def appendToDq(i):
-            # print(i, nums[i], ' => ', dq)
-            if not dq:                
-                dq.append(i)
-                # print('out 1', dq)
-                return
-            while len(dq) == k:
-                dq.popleft()
-            while dq and dq[0] <= i - k:
-                dq.popleft()
-
-            while dq and nums[dq[-1]] <= nums[i]:
-                dq.pop()
-            dq.append(i)
-            # print('out 2', dq)
-
-        for i in range(k-1):            
-            appendToDq(i)
-
-        n = len(nums)
         ret = []
-        for i in range(k-1, n):            
-            appendToDq(i)
+        q = deque()    
+        if len(nums) < k+1:
+            return [max(nums)]
+        for i, n in enumerate(nums):        
+            while q and nums[q[-1]] < n:
+                q.pop()
+            while q and (i - q[0]) >= k:
+                q.popleft()
+            q.append(i)
             
-            ret.append(nums[dq[0]])
+            if i + 1 >= k:
+                ret.append(nums[q[0]])
         return ret
-
