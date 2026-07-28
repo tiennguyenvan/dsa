419b573fdc6ea8e4138ef22935abde51b141f66e
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-07-28 12:11:48 -0700

    LC 649 Dota2 Senate

--- queue/Basic - Turn Simulation/LC 649 Dota2 Senate.py
+++ queue/Basic - Turn Simulation/LC 649 Dota2 Senate.py
@@ -1,34 +1,22 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        D = 0
-        R = 0
-        queue = list(senate)
-        remain = Counter(queue)        
-        pending = Counter()
+        D = deque()
+        R = deque()
+        n = len(senate)
+        for i, s in enumerate(senate):
+            if s == 'D':
+                D.append(i)
+                continue
+            R.append(i)
 
-        def winnerNameFound():
-            if remain['R'] <= 0 and remain['D'] > 0: return 'Dire'
-            if remain['D'] <= 0 and remain['R'] > 0: return 'Radiant'
-            return ''
-        def oppositeName(cur_name: str) -> str:
-            return 'D' if cur_name == 'R' else 'R'
+        while D and R:
+            D0 = D.popleft()
+            R0 = R.popleft()
+            banningDire = R0 < D0
+            if banningDire:
+                R.append(R0 + n)
+                continue
+            D.append(D0 + n)
+            
         
-        while not winnerNameFound():
-            qlen = len(queue)
-            for i in range(qlen):
-                name = queue.pop(0)
-                if pending[name] > 0:
-                    pending[name] -= 1
-                    remain[name] -= 1
-                    # print('misturn', name, remain, pending)
-                    continue
-                pending[oppositeName(name)] += 1
-                queue.append(name)
-                # print('power', name, pending, queue)
-        return winnerNameFound()
-                
-
-                
-        
-
-        
+        return 'Dire' if D else 'Radiant'
