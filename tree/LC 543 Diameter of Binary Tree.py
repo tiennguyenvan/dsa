0e7c5c9a599f8e5d42c60ade323b93b7321f49f2
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-07-29 08:22:26 -0700

    LC 543 Diameter of Binary Tree

--- tree/LC 543 Diameter of Binary Tree.py
+++ tree/LC 543 Diameter of Binary Tree.py
@@ -4,17 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class TreeStats:
+    def __init__(self, h, d):
+        self.h = h
+        self.d = d
+
 class Solution:
+    def _getTreeStats(self, node) -> TreeStats:
+        if not node:
+            return TreeStats(0,0)
+
+        leftStats = self._getTreeStats(node.left)
+        rightStats = self._getTreeStats(node.right)
+        return TreeStats(
+            max(leftStats.h, rightStats.h) + 1, 
+            max(leftStats.d, rightStats.d, leftStats.h + rightStats.h)
+        )
+
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        ret = 0
-        def dfs(root):
-            nonlocal ret
-            if not root: return 0
-            leftH = dfs(root.left)
-            rightH = dfs(root.right)
-            ret = max(ret, leftH+rightH)
-            return max(leftH,rightH)+1
+        rootStats = self._getTreeStats(root)
 
-        dfs(root)
-
-        return ret
+        return rootStats.d
+        
