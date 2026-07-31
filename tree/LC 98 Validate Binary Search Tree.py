84acd09e90b9d774d9fec36a78de155698005adb
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-07-30 17:33:34 -0700

    LC 98 Validate Binary Search Tree stack

--- tree/LC 98 Validate Binary Search Tree.py
+++ tree/LC 98 Validate Binary Search Tree.py
@@ -4,33 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Info:
-    def __init__(self, isBST, minVal = None, maxVal = None):
-        self.isBST = isBST
-        self.min = minVal
-        self.max = maxVal
-        # print(isBST, minVal, maxVal)
-
 class Solution:
-    def nodeInfo(self, node: Optional[TreeNode]) -> List[int]:
-        minVal = -float('inf')
-        maxVal = float('inf')
-
-        if not node:
-            return None            
-        l = self.nodeInfo(node.left)        
-        r = self.nodeInfo(node.right)
-
-        if not l: l = Info(True, maxVal, minVal)
-        if not r: r = Info(True, maxVal, minVal)                
-
-        return Info(
-            (l.isBST and r.isBST and l.max < node.val < r.min),
-            min(l.min, node.val),
-            max(r.max, node.val)
-        )     
-        
-
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        rootBstStat = self.nodeInfo(root)
-        return rootBstStat.isBST
+        pre = None
+        cur = root
+        stack = []
+        while cur or stack:
+            if cur:
+                stack.append(cur)
+                cur = cur.left
+                continue
+            cur = stack.pop()
+            if pre and pre.val >= cur.val:
+                return False
+            pre = cur
+            cur = cur.right
+        return True
