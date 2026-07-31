18e3e4d1336edd356d460cc0d66ec86fbd87cfa9
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-07-30 18:57:24 -0700

    LC 572 Subtree of Another Tree

--- tree/LC 572 Subtree of Another Tree.py
+++ tree/LC 572 Subtree of Another Tree.py
@@ -1,36 +1,20 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def treeSignature(seft, root):
-        vals = []
-        cur = root
+    def treeSignature(self, root):
+        signature = []
         stack = []
-        pre = None
+        cur = root
         while cur or stack:
             if cur:
-                stack.append(cur)                                                
+                left = '/' if cur.left else '['
+                right = '\\' if cur.right else ']'
+                signature.append(left + str(cur.val) + right)
+                stack.append(cur)
                 cur = cur.left
                 continue
-            peek = stack[-1]
-            if peek.right and peek.right != pre:
-                cur = peek.right
-                continue
-            pre = stack.pop()
-            vals.append('('+str(pre.val)+')')
-            if not pre.left:
-                vals.append('l')
-            if not pre.right:
-                vals.append('r')            
-        return '.'.join(vals)
+            cur = stack.pop()
+            cur = cur.right
+        return ''.join(signature)
+
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        subTreeSign = self.treeSignature(subRoot)
-        rootTreeSign = self.treeSignature(root)
-        # print(subTreeSign)
-        # print(rootTreeSign)
-        return subTreeSign in rootTreeSign
-        # return self.treeSignature(subRoot) in self.treeSignature(root)
-        
+        print(self.treeSignature(subRoot), self.treeSignature(root))
+        return self.treeSignature(subRoot) in self.treeSignature(root)
