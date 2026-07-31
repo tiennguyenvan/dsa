61f17e2df9dee5793edeaa1cad41ecff37901790
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-07-30 20:53:15 -0700

    LC 105 Construct Binary Tree from Preorder and Inorder Traversal

--- tree/LC 105 Construct Binary Tree from Preorder and Inorder Traversal.py
+++ tree/LC 105 Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -1,52 +1,23 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        v2i = {}
-        for i,v in enumerate(inorder):
-            v2i[v] = i
-        p = -1
-        def build(l,r):
-            if l >= r:
-                return
-            nonlocal p
-            p+=1
-            root = TreeNode(preorder[p])
-            m = v2i[root.val]
-            root.left = build(l, m)
-            root.right = build(m+1, r)
-            return root
-        return build(0, len(preorder))
+        iMap = {}
+        for i, v in enumerate(inorder):
+            iMap[v] = i
+        n = len(preorder)
 
+        def build(pstart, istart, length):            
+            if length <= 0: return None
+            val = preorder[pstart]
+            imid = iMap[val]
+            node = TreeNode(val)
+            leftStart = pstart + 1
+            leftLen = imid - istart
+            rightStart = leftStart + leftLen
+            righIStart = imid + 1
+            rightLen = istart + length - righIStart
+            node.left = build(leftStart, istart, leftLen)
+            node.right = build(rightStart, righIStart, rightLen)
+            return node
 
-        # v2i = {}
-        # for i,v in enumerate(inorder):
-        #     v2i[v] = i
-        # n = len(preorder)        
-        # def build(s,l,r):            
-        #     if l >= r:  
-        #         return None
-        #     v = preorder[s]
-        #     root = TreeNode(v)
-        #     m = v2i[v]          
-        #     ls = s+1
-        #     rs = s+1+m-l  
-        #     root.left = build(ls, l, m)                        
-        #     root.right = build(rs, m+1, r)            
-        #     return root
-        # return build(0, 0, n)
-
-
-
-        # if not preorder or not inorder:
-        #     return None
-        # root = TreeNode(preorder[0])
-        # index = inorder.index(root.val)
-        # root.left = self.buildTree(preorder[1:index+1], inorder[:index])
-        # root.right = self.buildTree(preorder[index+1:], inorder[index+1:])
-        # return root
-        
+        return build(0, 0, n)
