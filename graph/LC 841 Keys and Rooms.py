2f7e6af9debce1afeccea7ba81f13beba3b3eef9
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-08-31 08:32:21 -0700

    key and romm

---
+++ graph/LC 841 Keys and Rooms.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        unlocked = 1
+        n = len(rooms)
+
+        q = deque()
+        
+
+        return unlocked = n
