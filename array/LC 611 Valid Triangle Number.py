5408ecc7cb23d22e1dc29371001d923dbc3adf9d
Tim Nguyen <nguyentien.jobs@gmail.com>
2026-09-03 15:40:04 -0700

    LC 611 Valid Triangle Number

--- array/LC 611 Valid Triangle Number.py
+++ array/LC 611 Valid Triangle Number.py
@@ -1,3 +1,20 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        
+        sorted_nums = sorted(nums)
+        n = len(nums)
+        if n < 3: return 0
+
+        ret = 0
+        for i in range(2,n):
+            left = 0
+            right = i - 1
+            c = sorted_nums[i]
+            while left < right:
+                a,b = sorted_nums[left], sorted_nums[right]
+                if a + b <= c:
+                    left += 1
+                    continue
+                ret += right - left                
+                right -= 1
+
+        return ret
